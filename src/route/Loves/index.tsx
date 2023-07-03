00eba2ca406e2267bb0fd849dfee632f32d6a827
Archean-w <1091876908@qq.com>
2023-07-04 02:11:00 +0800

    我的喜欢 播放全部

--- src/route/Loves/index.tsx
+++ src/route/Loves/index.tsx
@@ -1,12 +1,12 @@
 import { Link, useRouteLoaderData } from "react-router-dom";
 import SongList from "../../components/SongList";
 import { InlineArtists } from "../../components/InlineArtists";
-import { Avatar, Box, Typography } from "@mui/material";
+import { Avatar, Box, Button, Stack, Typography } from "@mui/material";
 import { showTime } from "../../utils/formatter";
 import { startPlayback } from "../../api";
 import { usePlayerDevice } from "react-spotify-web-playback-sdk";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faCirclePlay, faHeart } from "@fortawesome/free-solid-svg-icons";
 import { useStore } from "../../store";
 import { useEffect } from "react";
 import { observer } from "mobx-react-lite";
@@ -20,6 +20,11 @@ function Loves() {
   useEffect(() => {
     store.lovesStore.setLoves();
   }, []);
+
+  function startPlay(index: number) {
+    startPlayback(`${userProfile.uri}:collection`, index, device?.device_id);
+  }
+
   return (
     <Box
       sx={{
@@ -35,22 +40,41 @@ function Loves() {
           alignItems: "center",
           mb: 2,
           borderRadius: 2,
-          background: "linear-gradient(to right, pink, #ff6b81)",
+          // background: "linear-gradient(to right, pink, #ff6b81)",
         }}>
         <Avatar
           variant="rounded"
           sx={{
             height: 200,
             width: 200,
-            background: "transparent",
+            background: "#f3f2f1",
           }}>
           <FontAwesomeIcon icon={faHeart} color={"red"} fontSize={80} />
         </Avatar>
-        <Typography
-          noWrap
-          sx={{ fontSize: 32, color: "white", fontWeight: "bolder" }}>
-          我的喜欢
-        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            height: "100%",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            gap: 1,
+          }}>
+          <Typography noWrap sx={{ fontSize: 32, fontWeight: "bolder" }}>
+            我的喜欢
+          </Typography>
+          <Stack direction={"row"} gap={2}>
+            <Button
+              onClick={() => startPlay(0)}
+              variant="contained"
+              color="success"
+              startIcon={<FontAwesomeIcon icon={faCirclePlay} />}
+              sx={{
+                maxWidth: 120,
+              }}>
+              播放全部
+            </Button>
+          </Stack>
+        </Box>
       </Box>
       <Box sx={{ flex: 1, overflow: "hidden" }}>
         <SongList
